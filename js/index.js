43413fd19a881b6b6f4e1eeb21f06070f2671d12
arsenicum32 <zombiehot@yandex.ru>
2016-04-06 21:13:33 +0300

    change

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ function rd(){
   return (5 + Math.floor(Math.random()*29)).toString();
 }
 setInterval(function(){
-  $('#sl').attr('src', 'slamp/images/'+rd()+'.png');
+  $('#sl').attr('src', 'images/'+rd()+'.png');
 },230)
